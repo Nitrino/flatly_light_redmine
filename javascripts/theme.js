4706fb8008a9b1880d54f71b2e77a265f4018388
Petr Stepchenko <nitrinodev@gmail.com>
2014-07-23 23:48:01 +0400

    clear javascript file

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,29 +1,11 @@
-
-
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
-
-/*jshint browser: true, strict: true, undef: true */
-
 ( function( window ) {
 
 'use strict';
-
-// class helper functions from bonzo https://github.com/ded/bonzo
 
 function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
 }
 
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
 if ( 'classList' in document.documentElement ) {
@@ -57,27 +39,20 @@ function toggleClass( elem, c ) {
 }
 
 window.classie = {
-  // full names
   hasClass: hasClass,
   addClass: addClass,
   removeClass: removeClass,
   toggleClass: toggleClass,
-  // short names
   has: hasClass,
   add: addClass,
   remove: removeClass,
   toggle: toggleClass
 };
 
-
-
-
 })( window );
 
 $(document).ready(function(){
-  // newdiv.setAttribute('style','position: absolute; left: 0; top: 0');
   $( '<div id="menu"><div class="burger"><div class="one"></div><div class="two"></div><div class="three"></div></div><div class="circle"></div></div>' ).insertBefore( $( "#top-menu" ) );
-  // wrapper3.appendChild(newdiv);
   
   var menuLeft = document.getElementById( 'top-menu' ),
   showLeft = document.getElementById( 'menu' ),
@@ -91,10 +66,7 @@ $(document).ready(function(){
     classie.toggle( menuButton, 'menu-push-toright' );
     classie.toggle( search, 'menu-push-toright' );
     classie.toggle( menuLeft, 'open' );
-    // disableOther( 'showLeft' );
   };
-  $('input[name$="q"]').attr('placeholder','Enter Search Text');
-  // $( "a" ).addClass( "fa" )
-  // $('body').addClass("menu-push-toright");
+  $( 'input[name$="q"]' ).attr( 'placeholder','Enter Search Text' );
 })
 
