5f0b2dabe22978ddd057b937f1c3e5c33538f3af
Petr Stepchenko <nitrinodev@gmail.com>
2018-01-27 20:23:09 +0300

    #59 disable redmine responsive

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -99,6 +99,35 @@
         $(document).ready(addElements)
       }
     }
-  }
+  };
 
+  function removeRule() {
+      if(typeof window.CSSMediaRule !== "function")
+          return false; //Your browser doesn't support media query feature
+
+      var s = document.styleSheets, r,
+          i, j, k;
+
+      if(!s) return false; //no style sheets found
+
+      // walk throuth css sheets
+      for(i=0; i<s.length; i++) {
+          // get all rules
+          r = s[i].cssRules;
+          if(!r) continue;
+
+          for(j=0; j<r.length; j++) {
+              //If there's a rule for media query
+              if(r[j] instanceof CSSMediaRule &&
+                      r[j].media.mediaText == "screen and (max-width: 899px)") {
+                  for(k=0; k<r[j].cssRules.length; k++) {
+                      // remove all rules of it
+                      r[j].deleteRule(r[j].cssRules[k]);
+                  }
+                  return true;
+              }
+          }
+      }
+  }
+  removeRule();
 })( window );
